eeba028fece91b219406dff010f84e597f648a7b
wunderszindi <sandrine.varga@gmail.com>
2019-02-11 11:46:42 +0100

    add.js changes

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 let a = 10;
 let text = 'asd';
+let c = true;
 
-function combine(a, b) {
-  return a + b;
+function combine (a, b, c) {
+  return a + b + c;
 }
+
+console.log(combine(a, text, c));
 
